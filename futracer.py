6ce9ad17f46e46b932b6ff6739a51907fd79396c
Niels G. W. Serup <ngws@metanohi.name>
2016-10-05 23:51:28 +0200

    It can render about 360 triangles without starting to lag.

--- futracer.py
+++ futracer.py
@@ -2,6 +2,7 @@
 
 import sys
 import math
+import random
 import argparse
 import itertools
 
@@ -34,6 +35,10 @@ class FutRacer:
         text = self.font.render(what, 1, (255, 255, 255))
         self.screen.blit(text, where)
 
+    def translate_point(self, move, point):
+        args = move + point
+        return self.futhark.translate_point_raw(*args)
+
     def rotate_point(self, angles, origo, point):
         args = angles + origo + point
         return self.futhark.rotate_point_raw(*args)
@@ -49,16 +54,49 @@ class FutRacer:
         s0 = [t0, t1]
         s1 = [[self.rotate_point((0.0, math.pi / 2, 0.0), origo, p) for p in t]
               for t in s0]
-        s2 = [[self.rotate_point((math.pi / 2, 0.0, 0.0), origo, p) for p in t]
+        s2 = [[self.rotate_point((0.0, -math.pi / 2, 0.0), origo, p) for p in t]
               for t in s0]
-        half_cube = s0 + s1 + s2
+        s3 = [[self.rotate_point((0.0, math.pi, 0.0), origo, p) for p in t]
+              for t in s0]
+        s4 = [[self.rotate_point((math.pi / 2, 0.0, 0.0), origo, p) for p in t]
+              for t in s0]
+        s5 = [[self.rotate_point((-math.pi / 2, 0.0, 0.0), origo, p) for p in t]
+              for t in s0]
+        half_cube_0 = s0 + s1 + s2 + s3 + s4 + s5
+
+        half_cube_0 = [[tuple(numpy.float32(x) for x in p) for p in t]
+                       for t in half_cube_0]
+
+
+        half_cubes = []
+        for i in range(30):
+            xm = random.random() * 1200.0
+            ym = random.random() * 1000.0
+            zm = random.random() * 1000.0
+            ax = random.random() * math.pi
+            ay = random.random() * math.pi
+            az = random.random() * math.pi
+            half_cube = [[self.rotate_point((ax, ay, az), origo, self.translate_point((xm, ym, zm), p)) for p in t]
+                         for t in half_cube_0]
+            half_cubes.extend(half_cube)
+        
+        p0s = [t[0] for t in half_cubes]
+        p1s = [t[1] for t in half_cubes]
+        p2s = [t[2] for t in half_cubes]
+
+        x0s = numpy.array([p[0] for p in p0s])
+        y0s = numpy.array([p[1] for p in p0s])
+        z0s = numpy.array([p[2] for p in p0s])
+
+        x1s = numpy.array([p[0] for p in p1s])
+        y1s = numpy.array([p[1] for p in p1s])
+        z1s = numpy.array([p[2] for p in p1s])
+
+        x2s = numpy.array([p[0] for p in p2s])
+        y2s = numpy.array([p[1] for p in p2s])
+        z2s = numpy.array([p[2] for p in p2s])
 
         camera = [[400.0, 300.0, 0.0], [0.0, 0.0, 0.0]]
-
-        print(half_cube)
-        half_cube = [[tuple(numpy.float32(x) for x in p) for p in t]
-                     for t in half_cube]
-        print(half_cube)
 
         keys_holding = {}
         for x in [pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT]:
@@ -70,25 +108,6 @@ class FutRacer:
 
             frame.fill(0)
 
-            half_cube = [[self.rotate_point((0.005, 0.01, 0.001), origo, p) for p in t]
-                         for t in half_cube]
-
-            p0s = [t[0] for t in half_cube]
-            p1s = [t[1] for t in half_cube]
-            p2s = [t[2] for t in half_cube]
-
-            x0s = numpy.array([p[0] for p in p0s])
-            y0s = numpy.array([p[1] for p in p0s])
-            z0s = numpy.array([p[2] for p in p0s])
-
-            x1s = numpy.array([p[0] for p in p1s])
-            y1s = numpy.array([p[1] for p in p1s])
-            z1s = numpy.array([p[2] for p in p1s])
-
-            x2s = numpy.array([p[0] for p in p2s])
-            y2s = numpy.array([p[1] for p in p2s])
-            z2s = numpy.array([p[2] for p in p2s])
-
             ((c_x, c_y, c_z), (c_ax, c_ay, c_az)) = camera
 
             frame = self.futhark.render_triangles_raw(
@@ -96,6 +115,9 @@ class FutRacer:
                 c_x, c_y, c_z, c_ax, c_ay, c_az).get()
 
             pygame.surfarray.blit_array(self.screen, frame)
+
+            self.message('FPS: {:.02f}'.format(fps), (10, 10))
+
             pygame.display.flip()
 
             # Check events.
@@ -115,18 +137,18 @@ class FutRacer:
 
             if keys_holding[pygame.K_UP]:
                 p1 = camera[0][:]
-                p1[2] += 5
+                p1[2] += 15
                 p2 = self.rotate_point(camera[1], camera[0], p1)
                 camera[0] = list(p2)
             if keys_holding[pygame.K_DOWN]:
                 p1 = camera[0][:]
-                p1[2] -= 5
+                p1[2] -= 15
                 p2 = self.rotate_point(camera[1], camera[0], p1)
                 camera[0] = list(p2)
             if keys_holding[pygame.K_LEFT]:
-                camera[1][1] -= 0.02
+                camera[1][1] -= 0.04
             if keys_holding[pygame.K_RIGHT]:
-                camera[1][1] += 0.02
+                camera[1][1] += 0.04
 
             self.clock.tick()
 
