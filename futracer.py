e30922cc52638b6ebd747a38db46c12f895c523d
Niels G. W. Serup <ngws@metanohi.name>
2017-01-13 17:10:27 +0100

    Preprocess triangles and textures in a separate function.

--- futracer.py
+++ futracer.py
@@ -12,6 +12,8 @@ import colorsys
 import pygame
 import numpy
 import png
+import pyopencl as cl
+import ctypes as ct
 
 import futracerlib
 
@@ -55,6 +57,103 @@ class FutRacer:
     def rotate_point(self, angles, origo, point):
         args = angles + origo + point
         return self.futhark.rotate_point_raw(*args)
+
+    def to_device(self, xs, typ):
+        # This is VERY brittle.  It must change whenever futhark-pyopencl
+        # changes.
+        xs_np = numpy.fromiter(xs, dtype=typ)
+        xs_mem = cl.Buffer(self.futhark.ctx, cl.mem_flags.READ_WRITE,
+                        numpy.long(numpy.int32(4) * xs_np.shape[0]))
+        cl.enqueue_copy(self.futhark.queue, xs_mem, xs_np,
+                        is_blocking=False)
+        xs = cl.array.Array(self.futhark.queue, xs_np.shape, typ,
+                            data=xs_mem)
+        return xs
+
+    def preprocess_triangles(self, triangles):
+        p0s = [t[0] for t in triangles]
+        p1s = [t[1] for t in triangles]
+        p2s = [t[2] for t in triangles]
+        ss = [t[3] for t in triangles]
+
+        x0s = self.to_device((p[0] for p in p0s), 'float32')
+        y0s = self.to_device((p[1] for p in p0s), 'float32')
+        z0s = self.to_device((p[2] for p in p0s), 'float32')
+
+        x1s = self.to_device((p[0] for p in p1s), 'float32')
+        y1s = self.to_device((p[1] for p in p1s), 'float32')
+        z1s = self.to_device((p[2] for p in p1s), 'float32')
+
+        x2s = self.to_device((p[0] for p in p2s), 'float32')
+        y2s = self.to_device((p[1] for p in p2s), 'float32')
+        z2s = self.to_device((p[2] for p in p2s), 'float32')
+
+        s_types = self.to_device((s[0] for s in ss), 'int32')
+        s_hsv_hs = self.to_device((s[1][0] for s in ss), 'float32')
+        s_hsv_ss = self.to_device((s[1][1] for s in ss), 'float32')
+        s_hsv_vs = self.to_device((s[1][2] for s in ss), 'float32')
+        s_indices = self.to_device((s[2] for s in ss), 'int32')
+
+        return (p0s, p1s, p2s, ss, x0s, y0s, z0s, x1s, y1s, z1s, x2s, y2s, z2s,
+                s_types, s_hsv_vs, s_hsv_ss, s_hsv_vs, s_indices)
+
+    def preprocess_textures(self, textures):
+        if len(textures) == 0:
+            # The values will not be used.
+            texture_w = 0
+            texture_h = 0
+        else:
+            # Assume all textures have the same size.  They must have!
+            # Otherwise something will fail later on.
+            t = textures[0]
+            texture_h, texture_w, _channels = t.shape
+
+        s_textures = numpy.array(textures)
+        s_textures_hs = s_textures[:,:,:,0]
+        s_textures_hs_flat = numpy.reshape(
+            s_textures_hs,
+            len(textures) * texture_h * texture_w)
+        s_textures_hs_flat = self.to_device(s_textures_hs_flat, 'float32')
+        s_textures_ss = s_textures[:,:,:,1]
+        s_textures_ss_flat = numpy.reshape(
+            s_textures_ss,
+            len(textures) * texture_h * texture_w).astype('float32')
+        s_textures_ss_flat = self.to_device(s_textures_ss_flat, 'float32')
+        s_textures_vs = s_textures[:,:,:,2]
+        s_textures_vs_flat = numpy.reshape(
+            s_textures_vs,
+            len(textures) * texture_h * texture_w).astype('float32')
+        s_textures_vs_flat = self.to_device(s_textures_vs_flat, 'float32')
+
+        return (len(textures), texture_w, texture_h,
+                s_textures_hs_flat, s_textures_ss_flat, s_textures_vs_flat)
+
+    def render_triangles_preprocessed(self, size, draw_dist, camera,
+                                      triangles_pre, textures_pre):
+        w, h = size
+
+        ((c_x, c_y, c_z), (c_ax, c_ay, c_az)) = camera
+
+        (p0s, p1s, p2s, ss, x0s, y0s, z0s, x1s, y1s, z1s, x2s, y2s, z2s,
+         s_types, s_hsv_hs, s_hsv_ss, s_hsv_vs, s_indices) = triangles_pre
+
+        (textures_len, texture_w, texture_h,
+         s_textures_hs_flat, s_textures_ss_flat, s_textures_vs_flat) = textures_pre
+
+        return self.futhark.render_triangles_raw(
+            w, h, self.draw_dist,
+            x0s, y0s, z0s, x1s, y1s, z1s, x2s, y2s, z2s,
+            s_types, s_hsv_hs, s_hsv_ss, s_hsv_vs, s_indices,
+            textures_len, texture_w, texture_h,
+            s_textures_hs_flat, s_textures_ss_flat, s_textures_vs_flat,
+            c_x, c_y, c_z, c_ax, c_ay, c_az)
+
+    def render_triangles(self, size, draw_dist, camera, triangles, textures):
+        triangles_pre = self.preprocess_triangles(triangles)
+        textures_pre = self.preprocess_triangles(textures)
+        return self.render_triangles_preprocessed(
+            size, draw_dist, camera,
+            triangles_pre, textures_pre)
 
     def random_cubes(self, square_texture=None):
         t0 = [(200.0, 100.0, 200.0),
@@ -173,7 +272,8 @@ class FutRacer:
         texture_w = 100
         texture_h = 100
         base_dir = os.path.dirname(__file__)
-        sonic_texture_raw = png.Reader(filename=os.path.join(base_dir, 'data/sonic-texture.png'))
+        sonic_texture_raw = png.Reader(filename=os.path.join(
+            base_dir, 'data/sonic-texture.png'))
         s_w, s_h, s_pixels, s_meta = sonic_texture_raw.asRGB8()
         assert s_w == texture_w
         assert s_h == texture_h
@@ -185,53 +285,17 @@ class FutRacer:
         sonic_texture = hsvs_3d
         triangles, textures = self.random_cubes(sonic_texture)
 
-        p0s = [t[0] for t in triangles]
-        p1s = [t[1] for t in triangles]
-        p2s = [t[2] for t in triangles]
-        ss = [t[3] for t in triangles]
-
-        x0s = numpy.array([p[0] for p in p0s]).astype('float32')
-        y0s = numpy.array([p[1] for p in p0s]).astype('float32')
-        z0s = numpy.array([p[2] for p in p0s]).astype('float32')
-
-        x1s = numpy.array([p[0] for p in p1s]).astype('float32')
-        y1s = numpy.array([p[1] for p in p1s]).astype('float32')
-        z1s = numpy.array([p[2] for p in p1s]).astype('float32')
-
-        x2s = numpy.array([p[0] for p in p2s]).astype('float32')
-        y2s = numpy.array([p[1] for p in p2s]).astype('float32')
-        z2s = numpy.array([p[2] for p in p2s]).astype('float32')
-
-        s_types = numpy.array([s[0] for s in ss]).astype('int32')
-        s_hsv_hs = numpy.array([s[1][0] for s in ss]).astype('float32')
-        s_hsv_ss = numpy.array([s[1][1] for s in ss]).astype('float32')
-        s_hsv_vs = numpy.array([s[1][2] for s in ss]).astype('float32')
-        s_indices = numpy.array([s[2] for s in ss]).astype('int32')
-        s_textures = numpy.array(textures)
-        s_textures_hs = s_textures[:,:,:,0]
-        s_textures_hs_flat = numpy.reshape(
-            s_textures_hs,
-            len(textures) * texture_h * texture_w).astype('float32')
-        s_textures_ss = s_textures[:,:,:,1]
-        s_textures_ss_flat = numpy.reshape(
-            s_textures_ss,
-            len(textures) * texture_h * texture_w).astype('float32')
-        s_textures_vs = s_textures[:,:,:,2]
-        s_textures_vs_flat = numpy.reshape(
-            s_textures_vs,
-            len(textures) * texture_h * texture_w).astype('float32')
+        # The objects will not change (only the camera changes), so we
+        # preprocess them to save important loop time.
+        triangles_pre = self.preprocess_triangles(triangles)
+        textures_pre = self.preprocess_textures(textures)
 
         while True:
             fps = self.clock.get_fps()
-            ((c_x, c_y, c_z), (c_ax, c_ay, c_az)) = camera
             time_start = time.time()
-            frame = self.futhark.render_triangles_raw(
-                self.size[0], self.size[1], self.draw_dist,
-                x0s, y0s, z0s, x1s, y1s, z1s, x2s, y2s, z2s,
-                s_types, s_hsv_hs, s_hsv_ss, s_hsv_vs, s_indices,
-                len(textures), texture_w, texture_h,
-                s_textures_hs_flat, s_textures_ss_flat, s_textures_vs_flat,
-                c_x, c_y, c_z, c_ax, c_ay, c_az)
+            frame = self.render_triangles_preprocessed(
+                self.size, self.draw_dist, camera,
+                triangles_pre, textures_pre)
             time_end = time.time()
             frame = frame.get()
             futhark_dur_ms = (time_end - time_start) * 1000
